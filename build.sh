745cade24ff7ae3d15bb7211e3b3b2c8edfbc38c
lujjjh <lujjjh@gmail.com>
2021-06-21 20:09:32 +0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+
+VERSION=9.1
+
+export PATH=$(pwd)/depot_tools:$PATH
+
+rm -rf v8
+mkdir v8
+cd v8
+fetch v8
+
+cd v8
+git checkout branch-heads/$VERSION
+gclient sync
+./build/install-build-deps.sh
+tools/dev/v8gen.py x64.release.sample
+ninja -C out.gn/x64.release.sample v8_monolith
