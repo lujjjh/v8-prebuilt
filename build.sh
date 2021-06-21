752a5066a5cf916428ace4b24ee895c31e0df275
lujjjh <lujjjh@gmail.com>
2021-06-21 21:25:41 +0800

    Try to fix the linux build

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ fetch v8
 cd v8
 git checkout branch-heads/$VERSION
 gclient sync
+sudo apt update && sudo apt remove php7.4-common && sudo apt autoremove
 ./build/install-build-deps.sh
 tools/dev/v8gen.py x64.release.sample
 ninja -C out.gn/x64.release.sample v8_monolith
